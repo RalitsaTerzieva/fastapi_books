b5304ce32122b787811e20262f6f91d78e5abde7
Ralica Popova <ralicaterzieva90@gmail.com>
2026-02-16 17:35:37 +0200

    Filter books by category query

--- books.py
+++ books.py
@@ -34,3 +34,12 @@ async def read_book(book_title: str):
         if book.get('title').casefold() == book_title.casefold():
             return book
         return {"message": "Book not found"}
+
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
