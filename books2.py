94e1f8309c7eac3e26e452ae868ca2e17ce50a2c
Ralica Popova <ralicaterzieva90@gmail.com>
2026-03-11 12:00:06 +0200

    Clean up code

--- books2.py
+++ books2.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -17,6 +18,17 @@ class Book:
         self.rating = rating
 
 
+class BookRequest(BaseModel):
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+
+
+
+
 BOOKS = [
     Book(1, "Clean Code", "Robert C. Martin", "A guide to writing clean and maintainable code.", 5),
     Book(2, "The Pragmatic Programmer", "Andrew Hunt", "Best practices and philosophy of software development.", 5),
@@ -29,3 +41,10 @@ BOOKS = [
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+
+@app.post("/create_book")
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(new_book)
+
